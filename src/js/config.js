3669cf22895bf780cb0f6b08e01baf983950d3a6
pandagroove <johann.vigroux@hotmail.com>
2022-02-24 11:34:30 +0100

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '723a7d50-8edd-4a0f-bc18-cb4b17f766b5';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/bookmarksView.js
@@ -0,0 +1,22 @@
+import View from './View.js';
+import previewView from './previewView.js';
+import icons from 'url:../../img/icons.svg';
+
+class BookmarksView extends View {
+  _parentElement = document.querySelector('.bookmarks__list');
+  _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it :)';
+  _message = '';
+
+  addHandlerRender(handler) {
+    window.addEventListener('load', handler);
+  }
+
+  _generateMarkup() {
+    // console.log(this._data);
+    return this._data
+      .map(bookmark => previewView.render(bookmark, false))
+      .join('');
+  }
+}
+
+export default new BookmarksView();
